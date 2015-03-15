89735a7a7da8d90e001d868dd1eb832170ce6691
marshl <leeism@live.com.au>
2015-03-15 22:27:41 +1100

    Removed defunct code Moved constants to their own file Moved some string functions to their own file

--- Hacking/main.cpp
+++ Hacking/main.cpp
@@ -12,84 +12,50 @@
 #include <sstream>
 #include <list>
 
-template <typename T> T clamp(const T& value, const T& low, const T& high) 
+#include "strfunc.h"
+#include "constants.h"
+
+template <typename T> T clamp( const T& _value, const T& _low, const T& _high ) 
 {
-  return value < low ? low : (value > high ? high : value); 
+	return _value < _low ? _low : ( _value > _high ? _high : _value ); 
 }
 
-using std::string;
-using std::vector;
-using std::list;
-
-const int LONGEST_PUZZLE_WORD = 15;
-//const unsigned int upperLimit = 23;
-
-const string introLines[] =
-{
-	"ROBCO INDUSTRIES (TM) TERMALINK PROTOCOL",
-	"!!!! WARNING: LOCKOUT IMMINENT !!!!",
-};
-const int INTRO_LINE_COUNT = sizeof(introLines) / sizeof(string);
-
-const int COLUMN_CHARACTER_WIDTH = 12;
-const int HEX_CODE_LENGTH = 6;
-
-const int COLUMN_COUNT = 2;
-const int ANSWER_AREA_WIDTH = LONGEST_PUZZLE_WORD + 1;
-const int TOTAL_COLUMN_WIDTH = HEX_CODE_LENGTH + 1 + COLUMN_CHARACTER_WIDTH + 1;
-const int TOTAL_SCREEN_WIDTH = TOTAL_COLUMN_WIDTH * COLUMN_COUNT + ANSWER_AREA_WIDTH;
-
-const int COLUMN_HEIGHT = 17;
-const int LINES_BEFORE_COLUMNS = INTRO_LINE_COUNT + 3;
-const int TOTAL_SCREEN_HEIGHT = COLUMN_HEIGHT + LINES_BEFORE_COLUMNS;
-
-const int TOTAL_COLUMN_LINE_COUNT = COLUMN_HEIGHT * COLUMN_COUNT;
-
-const COORD TOTAL_SIZE_COORD = { TOTAL_SCREEN_WIDTH, TOTAL_SCREEN_HEIGHT };
-const COORD ORIGIN_COORD = { 0, 0 };
 SMALL_RECT AREA_RECT = { ORIGIN_COORD.X, ORIGIN_COORD.Y, TOTAL_SCREEN_WIDTH, TOTAL_SCREEN_HEIGHT };
 
 CHAR_INFO* displayBuffer = new CHAR_INFO[TOTAL_SCREEN_HEIGHT * TOTAL_SCREEN_WIDTH];
 
-vector<string> characterBuffer( TOTAL_SCREEN_HEIGHT, string( TOTAL_SCREEN_WIDTH, ' ') );
-vector<vector<bool> > highlightBuffer( TOTAL_SCREEN_HEIGHT, std::vector<bool>( TOTAL_SCREEN_WIDTH, false ) );
+std::vector<std::string> characterBuffer( TOTAL_SCREEN_HEIGHT, std::string( TOTAL_SCREEN_WIDTH, ' ') );
+std::vector<std::vector<bool> > highlightBuffer( TOTAL_SCREEN_HEIGHT, std::vector<bool>( TOTAL_SCREEN_WIDTH, false ) );
 
-const WORD NORMAL_CHAR_ATTRIBUTES = FOREGROUND_GREEN | FOREGROUND_INTENSITY;
-const WORD HIGHLIGHTED_CHAR_ATTRIBUTES = BACKGROUND_GREEN | BACKGROUND_INTENSITY;
 
-const int STARTING_ATTEMPT_COUNT = 5;
 int attemptsRemaining = STARTING_ATTEMPT_COUNT;
-int startingPointer;
 
-const char* FILLER_CHARACTERS = "$-/?|=+&^%!@#&*{}[]()<>,.'\"";
-const int FILLER_CHARACTER_COUNT = strlen(FILLER_CHARACTERS);
-
-const int TOTAL_COLUMNS_CHARACTER_COUNT = COLUMN_COUNT * COLUMN_CHARACTER_WIDTH * COLUMN_HEIGHT;
-string stringBuffer( TOTAL_COLUMNS_CHARACTER_COUNT, '#' );
+std::string stringBuffer( TOTAL_COLUMNS_CHARACTER_COUNT, '#' );
 
 COORD cursorCoord = {0, 0};
 
-vector<string> hexAddresses( TOTAL_COLUMN_LINE_COUNT, std::string( HEX_CODE_LENGTH, '#' ) );
+std::vector<std::string> hexAddresses( TOTAL_COLUMN_LINE_COUNT, std::string( HEX_CODE_LENGTH, '#' ) );
 
 const int PUZZLE_WORD_LENGTH = 8;
+
 struct PuzzleWord
 {
-	string text;
+	PuzzleWord( std::string _text ) : text(_text), isHighlighted(false), isRemoved(false), position(-1) { }
+	std::string text;
 	bool isHighlighted;
+	bool isRemoved;
 	int position;
 	COORD screenCoords[PUZZLE_WORD_LENGTH];
 };
 
-const int puzzleDifference = 4;
-string solutionWord;
-vector<PuzzleWord*> puzzleWords;
+const int PUZZLE_DIFFERENCE = 4;
+std::string solutionWord;
+std::vector<PuzzleWord*> puzzleWords;
 
-vector<string> attemptedWords;
+std::vector<std::string> attemptedWords;
 
 PuzzleWord* currentHighlightedPuzzleWord;
 
-int StringDiff( const string& _str1, const string& _str2 );
-int StringDiff2( const string& _str1, const string& _str2 );
 void RefreshBuffer();
 void SwapBuffers();
 void SetHexAddresses();
@@ -97,16 +63,12 @@ void SetPuzzleWords();
 void PlacePuzzleWords();
 void OnClickEvent();
 void OnMouseMoveEvent();
+void GetSampleWordList( std::vector<std::string>& _out );
+COORD GetOutputCursorCoord();
 
 int main( int argc, char* argv[] )
 {
 	srand( (unsigned int)time( nullptr ) );
-
-	/*for ( int i = 0; i < TOTAL_SCREEN_HEIGHT; ++i )
-	{
-		characterBuffer[i].resize( TOTAL_SCREEN_WIDTH );
-		characterBuffer[i].replace( 0, TOTAL_SCREEN_WIDTH, TOTAL_SCREEN_WIDTH, '_' );
-	}*/
 
 	for ( int i = 0; i < TOTAL_COLUMNS_CHARACTER_COUNT; ++i )
 	{
@@ -117,28 +79,25 @@ int main( int argc, char* argv[] )
 	PlacePuzzleWords();
 	SetHexAddresses();
 
-	HANDLE inputHandle = CreateFile( "CONIN$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0 );
+	HANDLE inputHandle =  CreateFile( "CONIN$",  GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0 );
 	HANDLE outputHandle = CreateFile( "CONOUT$", GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0 );
-
-	assert( inputHandle != INVALID_HANDLE_VALUE );
-	assert( outputHandle != INVALID_HANDLE_VALUE );
 
 	SetConsoleActiveScreenBuffer( inputHandle );
 	DWORD flags;
 	GetConsoleMode( inputHandle, &flags );
 
 	DWORD fdwMode = flags & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT);
-	assert( SetConsoleMode(inputHandle, fdwMode) );
+	assert( SetConsoleMode( inputHandle, fdwMode ) );
 	
 	bool done = false;
 
-	while ( done == false )
+	while ( !done )
 	{
-		const int inputBufferSize = 255;
-		INPUT_RECORD inputBuffer[inputBufferSize];
+		const int INPUT_BUFFER_SIZE = 255;
+		INPUT_RECORD inputBuffer[INPUT_BUFFER_SIZE];
 		DWORD eventsRead;
 
-		if ( ReadConsoleInput( inputHandle, inputBuffer, inputBufferSize, &eventsRead ) )
+		if ( ReadConsoleInput( inputHandle, inputBuffer, INPUT_BUFFER_SIZE, &eventsRead ) )
 		{
 			unsigned int index = 0;
 			while ( index < eventsRead )
@@ -174,7 +133,7 @@ int main( int argc, char* argv[] )
 		RefreshBuffer();
 		SwapBuffers();
 		WriteConsoleOutput( outputHandle, displayBuffer, TOTAL_SIZE_COORD, ORIGIN_COORD, &AREA_RECT );
-
+		SetConsoleCursorPosition( outputHandle, GetOutputCursorCoord() );
 		Sleep( 1000 / 24 );
 	}
 
@@ -184,35 +143,6 @@ int main( int argc, char* argv[] )
 	}
 	delete displayBuffer;
 	return 0;
-}
-
-int StringDiff( const string& _str1, const string& _str2 )
-{
-	assert( _str1.length() == _str2.length() );
-
-	int differences = 0;
-	int l = _str1.length();
-	for ( int i = 0; i < l; ++i )
-	{
-		if ( _str1[i] != _str2[i] )
-		{
-			++differences;
-		}
-	}
-	return differences;
-}
-
-int StringDiff2( const string& _str1, const string& _str2 )
-{
-	int differences = 0;
-	for ( int i = 0; i < PUZZLE_WORD_LENGTH; ++i )
-	{
-		if ( _str1[i] != _str2[i] )
-		{
-			++differences;
-		}
-	}
-	return differences;
 }
 
 void SwapBuffers()
@@ -237,7 +167,7 @@ void RefreshBuffer()
 
 	std::ostringstream outstr;
 	outstr << attemptsRemaining << " ATTEMPT(S) LEFT:";
-	string str = outstr.str();
+	std::string str = outstr.str();
 
 	characterBuffer[INTRO_LINE_COUNT + 1].replace( 0, str.size(), str );
 
@@ -310,7 +240,7 @@ void RefreshBuffer()
 	else
 	{
 		std::ostringstream outstr;
-		outstr << "> " << string( PUZZLE_WORD_LENGTH, ' ' );
+		outstr << "> " << std::string( PUZZLE_WORD_LENGTH, ' ' );
 
 		characterBuffer[ TOTAL_SCREEN_HEIGHT - 1 ].replace( TOTAL_COLUMN_WIDTH * COLUMN_COUNT + 1,
 			outstr.str().length(),
@@ -334,86 +264,53 @@ void SetHexAddresses()
 
 void SetPuzzleWords()
 {
-	std::ifstream fin( "dictionary" );
-	if ( !fin.is_open() )
+	std::list<std::string> words;
+	std::vector<std::string> triedWords;
+	std::vector<std::string> goodWords;
+
+	GetSampleWordList( triedWords );
+	assert( triedWords.size() > WORD_COUNT );
+
+	std::list<std::string>::iterator iter = words.begin();
+
+	while ( true )
 	{
-		std::cout << "Error opening dictionary\n";
-		exit( 1 );
-	}
-
-	list<string> words;
-	string word;
-	
-	while ( fin >> word )
-	{
-		if ( word.length() == PUZZLE_WORD_LENGTH )
-		{
-			words.push_back( word );
-		}
-	}
-
-	fin.close();
-
-	int randIndex = rand() % words.size();
-
-	//string solutionWord = words[randIndex];
-	list<string>::iterator iter = words.begin();
-	std::advance( iter, randIndex );
-	solutionWord = *iter;
-
-	vector<string> triedWords;
-	vector<string> goodWords;
-	//std::cout << "Trying " << solutionWord << "\n";
-	for ( int i = 0; i < 10; )
-	{
-		//std::cout << "i:" << i << std::endl;
 		if ( words.size() == 0 )
 		{ // Ran out of usable words, reset and try again
 			words.insert( words.end(), triedWords.begin(), triedWords.end() );
 			triedWords.clear();
 			puzzleWords.clear();
+			goodWords.clear();
 
-			//std::cout << "Ran out of usable words: Resetting\n";
-			randIndex = rand() % words.size();
-			//solutionWord = words[randIndex];
 			iter = words.begin();
-			std::advance( iter, randIndex );
+			std::advance( iter, rand() % words.size() );
 			solutionWord = *iter;
-			//std::cout << "Trying " << solutionWord << "\n";
-			i = 0;
 		}
 
-		randIndex = rand() % words.size();
-		//string randWord = words[randIndex];
-
+		// Get a random word
 		iter = words.begin();
-		std::advance( iter, randIndex );
-		string randWord = *iter;
+		std::advance( iter, rand() % words.size() );
+		std::string randWord = *iter;
 
-		int diff = StringDiff2( randWord, solutionWord );
-		if ( diff <= puzzleDifference )
+		int diff = StringDiff( randWord, solutionWord );
+		if ( diff <= PUZZLE_DIFFERENCE )
 		{ // If within tolerances, add to the list
-			//std::cout << "Good word " << randWord << "\n";
 			goodWords.push_back( randWord );
-			++i;
+			if ( goodWords.size() >= WORD_COUNT - 1 ) 
+			{// We only need N-1 because the last word is the solution word
+				break;
+			}
 		}
 		// Add word to used words and remove from searchable list
 		triedWords.push_back( randWord );
-		//words.erase( words.begin() + randIndex );
 		words.erase( iter );
 	}
 
-	//for ( list<string>::iterator iter = goodWords.begin(); iter != goodWords.end(); ++iter )
-	for ( unsigned int i = 0; i < goodWords.size(); ++i )
+	puzzleWords.push_back( new PuzzleWord( solutionWord ) );
+	for ( unsigned int i = 0; i < WORD_COUNT - 1; ++i )
 	{
-		PuzzleWord* puzzleWord = new PuzzleWord();
-		//puzzleWord->text = *iter;
-		puzzleWord->text = goodWords[i];
-		puzzleWord->position = -1;
-		puzzleWord->isHighlighted = false;
-		puzzleWords.push_back( puzzleWord );
+		puzzleWords.push_back( new PuzzleWord( goodWords[i] ) );
 	}
-	
 }
 
 void PlacePuzzleWords()
@@ -423,12 +320,10 @@ void PlacePuzzleWords()
 	for ( unsigned int i = 0; i < puzzleWords.size(); )
 	{
 		// Theoretically, this might go infinite with the right conditions
-
 		int place = rand() % ( TOTAL_COLUMNS_CHARACTER_COUNT - PUZZLE_WORD_LENGTH );
 		bool badPlacement = false;
 		for ( unsigned int j = 0; j < i; ++j )
 		{
-			
 			if ( i != j 
 			  && puzzleWords[j]->position != -1 
 			  && std::abs( puzzleWords[j]->position - place ) < PUZZLE_WORD_LENGTH + 1 )
@@ -445,7 +340,6 @@ void PlacePuzzleWords()
 		}
 	}
 
-	
 	for ( unsigned int i = 0; i < puzzleWords.size(); ++i )
 	{
 		int place = puzzleWords[i]->position;
@@ -461,12 +355,12 @@ void PlacePuzzleWords()
 			y -= COLUMN_HEIGHT;
 		}
 
-		// If the string goes over the end of a line
+		// If the std::string goes over the end of a line
 		if ( x + PUZZLE_WORD_LENGTH > COLUMN_CHARACTER_WIDTH )
 		{
 			// Separate it into two
-			string chunk1 = puzzleWords[i]->text.substr( 0, COLUMN_CHARACTER_WIDTH - x + 1 );
-			string chunk2 = puzzleWords[i]->text.substr( COLUMN_CHARACTER_WIDTH - x + 1 );
+			std::string chunk1 = puzzleWords[i]->text.substr( 0, COLUMN_CHARACTER_WIDTH - x + 1 );
+			std::string chunk2 = puzzleWords[i]->text.substr( COLUMN_CHARACTER_WIDTH - x + 1 );
 			
 			for ( int j = 0; j < COLUMN_CHARACTER_WIDTH - x + 1; ++j )
 			{
@@ -511,7 +405,7 @@ void OnClickEvent()
 
 	if ( currentHighlightedPuzzleWord != nullptr )
 	{
-		int diff = StringDiff2( currentHighlightedPuzzleWord->text, solutionWord );
+		int diff = StringDiff( currentHighlightedPuzzleWord->text, solutionWord );
 
 		if ( diff == 0 )
 		{
@@ -549,4 +443,30 @@ void OnMouseMoveEvent()
 			}
 		}
 	}
+}
+
+void GetSampleWordList( std::vector<std::string>& _out )
+{
+	std::ifstream fin( "dictionary" );
+	if ( !fin.is_open() )
+	{
+		std::cout << "Error opening dictionary\n";
+		exit( 1 );
+	}
+	
+	std::string word;
+	while ( fin >> word )
+	{
+		if ( word.length() == PUZZLE_WORD_LENGTH )
+		{
+			_out.push_back( word );
+		}
+	}
+	fin.close();
+}
+
+COORD GetOutputCursorCoord()
+{
+	COORD c = { TOTAL_COLUMN_WIDTH * COLUMN_COUNT + 1, TOTAL_SCREEN_HEIGHT - 1 };
+	return c;
 }
